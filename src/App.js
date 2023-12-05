ba609ea591b45c2f477c172b7fd0ba7d201cec52
bstnprsx <bstnprs@hotmail.com>
2023-12-05 10:31:52 -0300

    change three

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1 style={{ color: 'white', fontSize: '4em' }}>Hola po Perrito!!!!!</h1>
         <h1 style={{ color: 'white', fontSize: '4em' }}>Hola po!!!!!</h1>
+        <h1 style={{ color: 'white', fontSize: '4em' }}>tu me conocí mucho!!!!!</h1>
       </header>
     </div>
   );
